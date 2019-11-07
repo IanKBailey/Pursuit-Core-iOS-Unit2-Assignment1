2ae3d47a219b3a79aaa7b4237402ef8f52b85530
Ian Bailey <iankbailey2@gmail.com>
2019-11-07 10:39:16 -0500

    function populates array with x's and o's

--- TicTacToe/TicTacToe/TicTacToeBrain.swift
+++ TicTacToe/TicTacToe/TicTacToeBrain.swift
@@ -8,6 +8,26 @@
 
 import Foundation
 
+
+var player1 = true
+var gameButton = GameButton()
+
+
+
 class TicTacToeBrain {
-  
+    var gameBoard = [["","",""],
+                     ["","",""],
+                     ["","",""]]
+
+    
+    func insertAtCoord (_ gameButton: GameButton) {
+        if player1 == true {
+            gameBoard[gameButton.row][gameButton.col] = "x"
+        } else {
+             gameBoard[gameButton.row][gameButton.col] = "o"
+        }
+    }
+    
 }
+
+
--- TicTacToe/TicTacToe/ViewController.swift
+++ TicTacToe/TicTacToe/ViewController.swift
@@ -10,11 +10,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    @IBOutlet var buttons: [GameButton]!
+    @IBOutlet weak var playerTurn: UILabel!
+    
+    
+   var gameBrain = TicTacToeBrain()
+    
+    
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
   }
 
-
+    @IBAction func buttonPushed(_ gameButton: GameButton) {
+        print("this is \(gameButton.row) row and \(gameButton.col) and ")
+        
+        let config = UIImage.SymbolConfiguration(pointSize: 25, weight: .black, scale: .large)
+        
+        let xmark = UIImage(systemName: "xmark", withConfiguration: config)
+        let omark = UIImage(systemName: "circle", withConfiguration: config)
+        
+        player1.toggle()
+        
+        if player1 == true {
+            gameButton.setImage(xmark, for: .normal)
+            gameButton.isEnabled = false
+            gameBrain.insertAtCoord(gameButton)
+            print(gameBrain.gameBoard)
+        } else {
+            gameButton.setImage(omark, for: .normal)
+            gameButton.isEnabled = false
+            gameBrain.insertAtCoord(gameButton)
+            print(gameBrain.gameBoard)
+        }
+        
+        
+    }
+   
+    @IBAction func reset(_ sender: UIButton) {
+    }
+    
+    
+    
+    
 }
 
